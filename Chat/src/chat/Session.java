63487e7405a9eef4457dadeb495f17104e02203f
ahmed <ahmed@ahmed-Lenovo-B40-80>
2016-02-29 22:09:47 +0200

    new  some changes

---
+++ Chat/src/chat/Session.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat;
+
+/**
+ *
+ * @author ahmed
+ */
+class Session {
+
+    static Session getSession(int aInt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getSessionId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
